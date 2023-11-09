d569d319021e8fca57c3c331a56f0c51ca51e74d
freeze0 <kras2001@bk.ru>
2023-11-09 15:34:01 +0300

    TopPersonController rework

--- Assets/Scripts/TopPersonController.cs
+++ Assets/Scripts/TopPersonController.cs
@@ -9,23 +9,18 @@ namespace ShadowChimera
 	{
 		[SerializeField] private InputActionAsset m_inputActionAsset;
 		[SerializeField] private CharacterController m_characterController;
-		[SerializeField] private Transform m_cameraTarget;
 		[SerializeField] private Transform m_cameraTransform;
 		
 		[SerializeField] private float m_rotationSmoothTime = 0.12f;
 		[SerializeField] private float m_speedChangeRate = 10f;
 		[SerializeField] private float m_moveSpeed = 5f;
 		[SerializeField] private float m_sprintSpeed = 10f;
-		[SerializeField] private float m_speedRotation = 40f;
-		[SerializeField] private float m_topClamp = 45f;
-		[SerializeField] private float m_bottomClamp = 0f;
 
 		private float m_cameraTargetYaw;
 		private float m_cameraTargetPitch;
 		private float m_rotationVelocity;
 		private float m_targetRotation;
-
-		//Input
+		
 		private InputActionMap m_playerMap;
 		private InputAction m_moveAction;
 		private InputAction m_lookAction;
@@ -35,7 +30,6 @@ namespace ShadowChimera
 		{
 			m_playerMap = m_inputActionAsset.FindActionMap("Player");
 			m_moveAction = m_playerMap.FindAction("Move");
-			m_lookAction = m_playerMap.FindAction("Look");
 		}
 
 		private void OnEnable()
@@ -52,12 +46,6 @@ namespace ShadowChimera
 		{
 			Vector2 move = m_moveAction.ReadValue<Vector2>();
 			Move(move, false);
-		}
-
-		private void LateUpdate()
-		{
-			Vector2 look = m_lookAction.ReadValue<Vector2>();
-			CameraRotation(look);
 		}
 
 		private void Move(Vector2 move, bool isSprint)
@@ -80,8 +68,7 @@ namespace ShadowChimera
 			    currentHorizontalSpeed > targetSpeed + speedOffset)
 			{
 				speed = Mathf.Lerp(currentHorizontalSpeed, targetSpeed * inputMagnitude, Time.deltaTime * m_speedChangeRate);
-
-				// round speed to 3 decimal places
+				
 				speed = Mathf.Round(speed * 1000f) / 1000f;
 			}
 			else
@@ -108,39 +95,6 @@ namespace ShadowChimera
 			Vector3 vertical = new Vector3(0f, Physics.gravity.y * Time.deltaTime, 0f);
 			Vector3 horizontal = targetDirection.normalized * (speed * Time.deltaTime);
 			m_characterController.Move(horizontal + vertical);
-		}
-
-        private void CameraRotation(Vector2 look)
-		{
-			const float threshold = 0.01f;
-			
-			if (look.sqrMagnitude >= threshold)
-			{
-				float deltaTimeMultiplier = m_speedRotation * Time.deltaTime;
-
-				m_cameraTargetYaw += look.x * deltaTimeMultiplier;
-				m_cameraTargetPitch += look.y * deltaTimeMultiplier;
-			}
-
-			m_cameraTargetYaw = ClampAngle(m_cameraTargetYaw, float.MinValue, float.MaxValue);
-			m_cameraTargetPitch = ClampAngle(m_cameraTargetPitch, m_bottomClamp, m_topClamp);
-
-			m_cameraTarget.rotation = Quaternion.Euler(50, 20, 0f);
-		}
-
-        private static float ClampAngle(float angle, float min, float max)
-		{
-			if (angle < -360f)
-			{
-				angle += 360f;
-			}
-
-			if (angle > 360f)
-			{
-				angle -= 360f;
-			}
-
-			return Mathf.Clamp(angle, min, max);
 		}
 	}
 }
